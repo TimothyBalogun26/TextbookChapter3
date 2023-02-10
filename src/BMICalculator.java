0b1032f96deaedaf83b44c5cafeef621a242d4d3
Timyn <tbalogun@ggc.edu>
2023-02-09 19:28:09 -0500

    Chapter 3 Commit

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class BMICalculator {
+/**
+ * @class: BMICalculator
+ * @author: Timothy Balogun
+ * @course: ITEC 2140-04, Spring 2023
+ * @written: February 6, 2023
+ * @description: In this program the script prompts the user to enter his/her weight in pounds and his/her height in inches. the program
+  then calculates the bmi using the formula: BMI = Weight(kilograms)/(height(meters))2.
+ */
+
+import java.util.Scanner;
+public class BMICalculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+// When prompted you will enter weight in pounds
+        System.out.print("Enter your weight in pounds: ");
+        double weightInPounds = input.nextDouble();
+// When prompted you will enter the height in inches
+        System.out.print("Enter your height in inches: ");
+        double heightInInches = input.nextDouble();
+// Equation to convert pounds to weight in kg
+        double weightInKg = weightInPounds * 0.4536;
+// Equation to convert height in inches to height in meters
+        double heightInMeters = heightInInches * 0.0254;
+// Equation to find the bmi
+        double bmi = weightInKg / (heightInMeters * heightInMeters);
+
+        System.out.println("Your BMI is equal to: " + bmi);
+// These boolean values are used to determine whether one's weight is Underweight, Normal weight, Overweight, Obese
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal weight");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+
+    }
 }
--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TriangleCalculator {
+/**
+ * @class: TriangleCalculator
+ * @authour: Timothy Balogun
+ * @course: ITEC 2140-04, Spring 2023
+ * @written: February 6, 2023
+ * @description: The program calculates the perimeter of the triangle if the input is valid, otherwise it prints invalid input.
+   The input is valid if the sum of every pair of two edges is greater than the remaining edge.
+ */
+
+import java.util.Scanner;
+public class TriangleCalculator {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the first edge of the triangle: ");
+        // Prompts users to input the measures of edge: A,B, and C
+        int edgeA = scanner.nextInt();
+
+        System.out.print("Enter the length of the second edge of the triangle: ");
+        int edgeB = scanner.nextInt();
+
+        System.out.print("Enter the length of the third edge triangle: ");
+        int edgeC = scanner.nextInt();
+        // This is a boolean evaluation to determine if the statement below is correct or not
+        if ((edgeA + edgeB > edgeC) && (edgeB + edgeC > edgeA) && (edgeA + edgeC > edgeB)) {
+            // This is the equation to find the perimeter of the edges
+            int perimeterOfTriangle = edgeA + edgeB + edgeC;
+            System.out.println("The perimeter of the triangle is equal to: " + perimeterOfTriangle);}
+        // If the boolean value above is incorrect the user will then be prompted with the value below
+        else {
+            System.out.println("Invalid input");
+        }
+    }
 }
